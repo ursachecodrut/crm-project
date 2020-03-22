37f8cb5ed2e0675e9eb8834266350adea6a62b0e
Ursache Codrut-Stefan <ursache.codrut71@gmail.com>
2020-03-22 20:58:16 +0200

    static files commit

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -104,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -119,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'static'),
+]
+
